15fc19a89aabc9363549e16919d4a0fd10e6c843
Simon Edkins <simon.edkins@orange.com>
2013-04-25 23:00:41 +0200

    tidied up a bit

--- hundred_doors.py
+++ hundred_doors.py
@@ -1,8 +1,6 @@
 '''Solution to the 100 Doors Problem'''
 from jinja2 import Environment, PackageLoader
 
-
-
 class Door():
 	'''Door Object'''
 	door_id = None
@@ -28,7 +26,7 @@ class Door():
 
 
 def build_doors():	
-	'''function to build the Doorsi, returns a list of doors'''
+	'''function to build the Doors, returns a list of doors'''
 	#Create 100 closed doors
 	n_doors = 100
 	doors = []
@@ -48,46 +46,28 @@ def pass_doors(doors, pass_number):
 	return doors
 
 
-
-
-
-
-
 def do_doors():
+	'''Actually perform and output the calculation'''
 	n_passes = 100
 	doors = build_doors()
 	pass_list = []
 	pass_open_count = []
-
 	pass_number = 0
-
 
 	for pass_number in range(1,n_passes+1):
 		doors = pass_doors(doors,pass_number)
 		pass_list.append([door.is_open for door in doors])
 		pass_open_count.append(sum([1 for door in doors if door.is_open==True]))
-	
 
-
-
-
-	print pass_list
-
+	#Output the solution to html template
 	env = Environment(loader=PackageLoader('hundred_doors','templates'))
 
 	template = env.get_template('door_template.html')
 
 	html_string = template.render({'pass_list':pass_list, 'pass_open_count':pass_open_count,'enumerate':enumerate})
-
 	
 	with open('doors_solution.html','w') as html_file:
 		html_file.write(html_string)
-
-
-
-
-
-
 	
 if __name__ == '__main__':
 
